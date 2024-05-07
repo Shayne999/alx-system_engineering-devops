c3ddea3a75a268cc2e50e78f2722af32029ebb19
Shayne999 <shaynejuniorn@gmail.com>
2024-05-07 13:35:36 +0200

    Complete

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 
 """ queries the Reddit API and returns a list
-containing the titles of all hot articles for a given subreddit.
+containing the titles of all hot articles for a given subreddit..
 """
 
 import requests
-import sys
 
-def recurse(subreddit, hot_list=None, after=None, count=0):
-    """ returns a list containing the titles of all hot articles """
-    if hot_list is None:
-        hot_list = []
-    url = "http://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-            "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/shayne_ndlovu)"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
-            "after": after,
-            "count": count,
-            "limit": 100
+        "after": after,
+        "count": count,
+        "limit": 100
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
@@ -29,10 +27,9 @@ v1.0.0 (by /u/shayne_ndlovu)"
     results = response.json().get("data")
     after = results.get("after")
     count += results.get("dist")
-    for child in results.get("children"):
-        hot_list.append(child.get("data").get("title"))
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
     return hot_list
-    
