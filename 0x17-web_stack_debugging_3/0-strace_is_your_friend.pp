d48752992d891ae47be78862b7f59cdbc4437615
Shayne999 <shaynejuniorn@gmail.com>
2024-05-07 20:26:45 +0200

    Complete

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes the typo in wordpress to fix Apache 500 error
+
+exec {'fix typo':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+This directory contains a file with puppet code. The code in the file is the solution to the Alx 0x17-web_stack_debugging_3 task.
